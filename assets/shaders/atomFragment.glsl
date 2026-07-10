1da110fd64ad8615fcabb0a77628acd7c389ede0
Ronie Salgado <ronie@desromech.cl>
2026-07-09 20:42:39 -0400

    Fixing impostor rendering.

--- assets/shaders/atomFragment.glsl
+++ assets/shaders/atomFragment.glsl
@@ -1,55 +1,26 @@
 #line 2
 
 layout(location=0) flat in uint inAtomIndex;
-layout(location=1) in vec3 inViewPosition;
-layout(location=2) in vec3 inViewCenter;
+layout(location=1) in vec2 inTexcoord;
+layout(location=2) in vec3 inViewPosition;
 
 layout(location=0) out vec4 outFragColor;
 
-layout (depth_less) out float gl_FragDepth; // To keep early-z test.
-
-bool raySphereTest(float sphereRadius, in vec3 sphereCenter, in vec3 rayDirection, out vec2 lambdas)
-{
-    // Ray sphere intersection formula from: https://viclw17.github.io/2018/07/16/raytracing-ray-sphere-intersection
-    vec3 rayOriginSphereCenter = vec3(0.0) - sphereCenter;
-    float a = dot(rayDirection, rayDirection);
-    float b = 2.0 * dot(rayDirection, rayOriginSphereCenter);
-    float c = dot(rayOriginSphereCenter, rayOriginSphereCenter) - sphereRadius*sphereRadius;
-    float delta = b*b - 4.0*a*c;
-    if (delta < 0.0)
-        return false;
-
-	float deltaSqrt = sqrt(delta);
-    lambdas = vec2(-b - deltaSqrt, -b + deltaSqrt) / (2.0*a);
-
-    return true;
-}
+//layout (depth_less) out float gl_FragDepth; // To keep early-z test.
 
 void main()
 {
     AtomRenderingState atomState = AtomRenderingStateBuffer[inAtomIndex];
-    vec3 D = normalize(inViewPosition);
-
-    vec2 lambdas;
-    //raySphereTest(desc.radius, viewCenter, D, lambdas);
-    bool inside = raySphereTest(atomState.radius, inViewCenter, D, lambdas);
-    if (!inside)
+    bool isInside = length(inTexcoord) <= 1.0;
+    if(!isInside)
         discard;
 
-    // Compute the intersection point.
-    vec3 P = D*min(lambdas.x, lambdas.y);
-
-    // Compute the intersection point depth.
-    vec4 clipPosition = CameraState.projectionMatrix * vec4(P, 1.0);
-    gl_FragDepth = clipPosition.z / clipPosition.w;
-
-    // Compute the normal at the intersection point.
-    vec3 N = normalize(P - inViewCenter);
-    vec3 V = -D;
-
+    vec3 N = normalize(vec3(inTexcoord, 1.0));
+    vec3 V = normalize(-inViewPosition);
     float NdotV = max(0.0, dot(N, V));
-    //outFragColor = vec4(D*0.5 + 0.5, 1.0); 
-    //outFragColor = atomState.color;
+    
+    //outFragColor = vec4(inTexcoord*0.5 + 0.5, 0.0, 1.0);
+    //outFragColor = vec4(N*0.5 + 0.5, 1.0);
     vec4 baseColor = atomState.color;
     outFragColor = vec4(baseColor.rgb*(0.2 + NdotV*0.8), baseColor.a);
 }
--- assets/shaders/atomVertex.glsl
+++ assets/shaders/atomVertex.glsl
@@ -1,14 +1,14 @@
 #line 2
 
 layout(location = 0) flat out uint outAtomIndex;
-layout(location = 1) out vec3 outViewPosition;
-layout(location = 2) out vec3 outViewCenter;
+layout(location = 1) out vec2 outTexcoord;
+layout(location = 2) out vec3 outViewPosition;
 
 const vec2 quadVertices[4] = vec2[4](
     vec2(-1.0, -1.0),
-    vec2(-1.0, 1.0),
-    vec2(1.0, -1.0),
-    vec2(1.0, 1.0)
+    vec2(-1.0,  1.0),
+    vec2( 1.0, -1.0),
+    vec2( 1.0,  1.0)
 );
 
 void main()
@@ -21,8 +21,9 @@ void main()
 
     vec3 modelCenter = atomPosition;
     vec3 worldCenter = (ModelState.modelMatrix *vec4(modelCenter, 1.0)).xyz;
-    outViewCenter = (CameraState.viewMatrix*vec4(worldCenter, 1.0)).xyz;
-    outViewPosition = outViewCenter + vec3(quadVertices[gl_VertexIndex], 1.0)*atomRadius;
+    vec3 viewCenter = (CameraState.viewMatrix*vec4(worldCenter, 1.0)).xyz;
+    outViewPosition = viewCenter + vec3(quadVertices[gl_VertexIndex], 1.0)*atomRadius;
+    outTexcoord = quadVertices[gl_VertexIndex];
 
     gl_Position = CameraState.projectionMatrix*vec4(outViewPosition, 1.0);
 }
